ba095670eb351148cd132fd16deeb5cb510f69c3
Javier Villafuerte <javier@DarthVader.local>
2016-04-21 16:59:42 -0500

    removing select state from previosly selected items

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -155,10 +155,10 @@ View.prototype.selectRoute = function(e) {
 
     });
   } else {
-    $(el).removeClass('route-selected');
+    $(el).parent().find('.RouteCard').removeClass('route-selected');
     var itineration = JSON.parse(localStorage.getItem('itineration'));
    for (var i=0; i<itineration.length;i++) {
-       if (i!=model.index){
+       if (i!=this.model.index){
           var rec2 = d3.selectAll(".circle-fade-"+i);
           rec2.attr('class', 'leaflet-marker-icon leaflet-div-icon1 circle-fade-'+i+ ' leaflet-zoom-hide');
        }
