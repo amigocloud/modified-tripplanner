05df3e748821e130a0e688a642299538fc536b92
Javier Villafuerte <javier@DarthVader.local>
2016-04-26 15:34:05 -0500

    working on bus stops drawing

--- client/route-card-view/index.js
+++ client/route-card-view/index.js
@@ -21,7 +21,7 @@ var mapView = require('map-view');
 var View = module.exports = view(require('./template.html'), function (view, model) {
     view.isSelected = false;
     mouseenter(view.el, function () {
-        if (optionsView.lastCardSelected && optionsView.lastCardSelected.model.index !== this.model.index) {
+        if (optionsView.lastCardSelected && optionsView.lastCardSelected.model.index !== view.model.index) {
             console.log(optionsView.lastCardSelected);
             mapView.removeRouteStops();
         }
